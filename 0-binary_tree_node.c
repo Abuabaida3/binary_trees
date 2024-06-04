df6ba386fa3410927f361ac9cf3f67196e3051e3
Sarah441 <sarahibrahim3477@gmail.com>
2024-06-05 00:50:12 +0300

    ff

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,11 +2,11 @@
 /**
  * binary_tree_node - create a binary node
  * @parent: node parent
- * Return: the new node
+ * Return: the new node we created
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_node = NULL;
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == (NULL))
 		return (NULL);

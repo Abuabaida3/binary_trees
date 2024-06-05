25ca1b2ccc63b179e3e7a894b587941218a7d32f
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-06-05 14:27:39 -0700

    my first

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * size_t binary_tree_leaves function that retun the number of leaves in a tree
+ * @tree: tree to check
+ * Return: number of leaves
+ */
+size_t binary_tree_leave(const binary_tree_t *tree);
+{
+	if (tree == NULL);
+	{
+		return (0)
+	}
+	else
+	{
+		binary_ tree_leaves(tree->lift);
+		binary_tree_leaves(tree->right);
+		return (leaf)
+	}
+}
+			

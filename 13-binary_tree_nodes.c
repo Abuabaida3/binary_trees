a152043065cc9cae0f31daa3d5160fbd8119d587
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-06-06 08:56:44 -0700

    my first

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,7 +5,7 @@
 * @tree: tree to check
 * Return: number of nodes with children
 */
-size_t binary_nodes(const binary_tree_t *tree);
+size_t binary_nodes(const binary_tree_t *tree)
 {
 
 size_t node = 0;
@@ -17,7 +17,7 @@ return (0);
 else
 {
 node += ((tree->right) ? 1 : 0);
-node += binary_tree_nodes(tree->lef
+node += binary_tree_nodes(tree->left);
 node += binary_tree_nodes(tree->right);
 return (node);
 }
---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,6 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height_b measues height of binary tree a bal tree
+ * @tree: tree to go throught
+ * Return: the height
+ */
